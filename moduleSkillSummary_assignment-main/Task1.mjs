bfe0b740e516174daedced1d4f0a12d87a6f8324
Andreas2298 <69863018+Andreas2298@users.noreply.github.com>
2025-02-23 12:31:36 +0100

    Created a function that returns a length in mm assuming it has been given a length in inches.

--- moduleSkillSummary_assignment-main/Task1.mjs
+++ moduleSkillSummary_assignment-main/Task1.mjs
@@ -6,3 +6,11 @@ function squareNumber(num) {
   return num * num;
 }
 console.log(squareNumber(8));
+
+
+//2. A function that returns a length in mm assuming it has been given a length in inches.
+
+function convertToMm(inches) {
+  return inches * 25.4;
+}
+console.log(convertToMm(4));
