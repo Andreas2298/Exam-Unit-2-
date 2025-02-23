18e74eb55ac3ba9e3cef28dd9ee7fab531c40eb6
Andreas2298 <69863018+Andreas2298@users.noreply.github.com>
2025-02-23 13:37:54 +0100

    Implemented recursive array flattening.
    Created a for loop that iterates over each of the elements (items) in the array that was passed to flatArray. Then I used a conditional where an if statement was created to check whether the current item is an array that uses array.isArray(item). If it is an array, the function will call flatArray(item) and then flatten the nested structure by using a spread operator (...). When all the elements are proccessed, then arrayResult will be returned as a flattened array.

--- moduleSkillSummary_assignment-main/Task2.mjs
+++ moduleSkillSummary_assignment-main/Task2.mjs
@@ -2,3 +2,13 @@
 
 function flatArray(array) {
   let arrayResult = [];
+for (let item of array) {
+    if (Array.isArray(item)) {
+      arrayResult.push(...flatArray(item));
+    } else {
+      arrayResult.push(item);
+    }
+  }
+
+  return arrayResult;
+}
