da0036bcceccfa62c1d35533aa099e1d447b52c1
Andreas2298 <69863018+Andreas2298@users.noreply.github.com>
2025-02-23 19:03:25 +0100

    Fixed code by removing built-in functions .pop() and .push().

--- moduleSkillSummary_assignment-main/Task3.mjs
+++ moduleSkillSummary_assignment-main/Task3.mjs
@@ -9,22 +9,27 @@ function sumNodes(filename) {
 
   const nodesRoot = JSON.parse(data);
 
-  let totalSumStructure = 0;
-  const nodeStack = [nodesRoot];
+  let totalSumFullStack = 0;
+  let nodeStack = [];
+  nodeStack[0] = nodesRoot;
 
   while (nodeStack.length > 0) {
-    const currentNode = stack.pop();
+    const currentNode = nodeStack[nodeStack.length - 1];
+    nodeStack.length = nodeStack.length - 1;
 
-    if (currentNode.right) {
-      nodeStack.push(currentNode.left);
+    if (currentNode) {
+      totalSumFullStack += currentNode.value;
+
+      if (currentNode.right) {
+        nodeStack[nodeStack.length] = currentNode.right;
+      }
       if (currentNode.left) {
-        nodeStack.push(currentNode.left);
+        nodeStack[nodeStack.length] = currentNode.left;
       }
     }
   }
 
-  return totalSumStructure;
+  return totalSumFullStack;
 }
-
 const totalSumStructure = sumNodes("example_files/nodes.json");
 console.log("The total sum of all node values are:", totalSumStructure);
